48fd183dc27657d3f5c7dbe8d278073f14f7bb5c
Checkmate <dag368@cornell.edu>
2024-01-17 13:15:59 -0700

    Implemented sum_even

--- loops.py
+++ loops.py
@@ -3,4 +3,8 @@ def sum_evens(lst):
     Returns the sum of even integers in the given integer list `lst`
     Note that a negative number is even if its absolute value is even
     """
-    pass
+    result = 0
+    for number in lst:
+        if number % 2 == 0:
+            result += number
+    return result
